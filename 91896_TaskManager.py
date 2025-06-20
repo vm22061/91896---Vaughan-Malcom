aa81a4dcf8f23ad99c84110780497c8cb4ac416f
vm22061 <vm22061@my.westlake.school.nz>
2025-06-20 19:13:22 +1200

    I had got partially stumped on the add new task so I have now begun to create the print_tasks function with a sorter (no bugs but unfinished code)

--- 91896_TaskManager.py
+++ 91896_TaskManager.py
@@ -59,23 +59,22 @@ team_member_dictionary = {
 def add_new_task():
     """"Using this function, the users will be able to add a new task to the 
         task list, assign a team member, priority status and discription"""
-    edit_add_multenterbox(["","","","",""],task_dictionary,"Add a new task")
+    edit_or_add_multenterbox(["","","","",""],task_dictionary,"Add a new task")
 
-def edit_add_multenterbox(initial_values, accessing_dictionary,title):
+def edit_or_add_multenterbox(initial_values, accessing_dictionary,title):
     """"IDK"""
     prompt = "Please enter the desired information below..."
     feild_value_types = []
     entering_fields = []
     for main_dictionary_id, dictionary_definitions in accessing_dictionary.items():
-        feild_value_types.append(type(dictionary_definitions))
         for dictionary_key in dictionary_definitions:
             if dictionary_key not in entering_fields:
                 entering_fields.append(dictionary_key)
     print(feild_value_types)            
     task_details = easygui.multenterbox(prompt,title,entering_fields,
         initial_values)
-    #if task_details == None:
-    #    main_menu()
+    if task_details == None:
+        main_menu()
     #for entered_value in task_details:
     #    if entered_value == None:
     #        print("None")
@@ -86,13 +85,37 @@ def edit_add_multenterbox(initial_values, accessing_dictionary,title):
 
 #def generate_report():
 
-#def print_tasks():
+def print_tasks(sort_status, sort_direction):
+    """"Print tasks"""""
+    choicebox_title = "Current tasks"
+    choicebox_text = [f"Sorted {sort_status}:"]
+    choicebox_text.append("==============================================================")
+    choicebox_sort_options = ["By Creation", "By Priority", "By Status", "By Assignee", "Exit"]
+
+    if sort_status == "By Creation":
+        for task_id, task_details in task_dictionary.items():
+            choicebox_text.append(f"\n[ {task_dictionary[task_id]['Title']} ]")
+            for task_key, task_value in task_details.items():
+                if task_key != "Title":
+                    choicebox_text.append(f"\t{task_key}: {task_value}")
+    
+    sort_or_exit = easygui.choicebox("\n".join(choicebox_text), choicebox_title, choices=choicebox_sort_options)
+    if sort_or_exit == None or sort_or_exit == "Exit":
+        main_menu()
+    else:
+        buttonbox_text = "Sort by: " + sort_or_exit
+        buttonbox_choices = ["Ascending", "Descending", "Exit"]
+        sort_direction_choice = easygui.buttonbox(buttonbox_text, choices=buttonbox_choices)
+        if sort_direction == None or sort_direction == "Exit":
+            print_tasks(sort_status, sort_direction)
+        else:
+            print_tasks(sort_or_exit, sort_direction_choice)
 
 #def check_if_digit(digit,max_value,min_value):
 
 #def check_if_string(string):
 
 def main_menu():
-    print("Main menu")
+    print_tasks("By Creation", "Ascending")
 
-add_new_task()
+main_menu()
